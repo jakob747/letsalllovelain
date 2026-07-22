768f5846a76992927cd6e57a36cf72ab6635738e
jakob747 <linkonjakob@gmail.com>
2026-07-22 11:17:03 +0530

    Update Firebase configuration in config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDaYHL69dfsxfACQxCHaS1jqIA6rrQvKEA",
+  authDomain: "wiredforum-be8ec.firebaseapp.com",
+  projectId: "wiredforum-be8ec",
+  storageBucket: "wiredforum-be8ec.firebasestorage.app",
+  messagingSenderId: "34110539084",
+  appId: "1:34110539084:web:25bd3f5eb4bcd651dcfbe9"
 };
